30d1418347632722cd0b5717463637ef33c07448
yann <yann.boisselier@gmail.com>
2018-10-13 21:05:02 +0200

    rename config gile

--- src/config.rs
+++ src/config.rs
@@ -2,14 +2,16 @@ use rs_config::*;
 use std::collections::HashMap;
 use std::path::Path;
 
-/// Récupère la configuration du fichier `App.toml`
-/// Si un fichier `App.local.toml` est trouvé, ses valeurs
-/// écrasent celle de `App.toml`
+/// Récupère la configuration du fichier `Config.toml`
+/// Si un fichier `Config.local.toml` est trouvé, ses valeurs
+/// écrasent celle de `Config.toml`
 pub fn get() -> HashMap<String, String> {
   let mut settings = Config::default();
-  settings.merge(File::with_name("App.toml")).unwrap();
-  if Path::new("App.local.toml").exists() {
-    settings.merge(File::with_name("App.local.toml")).unwrap();
+  settings.merge(File::with_name("Config.toml")).unwrap();
+  if Path::new("Config.local.toml").exists() {
+    settings
+      .merge(File::with_name("Config.local.toml"))
+      .unwrap();
   }
   let config = settings.try_into::<HashMap<String, String>>().unwrap();
   config
